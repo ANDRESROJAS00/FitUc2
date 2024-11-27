7a5d7767902afb9f55ec4a4d0470935103645117
Andres Rojas <andre.rojasm@duocuc.cl>
2024-11-27 20:54:35 -0300

    configuracion appflow 3

--- env-config.js
+++ env-config.js
@@ -1,18 +1,56 @@
 const fs = require('fs');
-const environment = process.env.ENV;
 
-const targetPath = `./src/environments/environment.${environment}.ts`;
-const envConfigFile = `
-export const environment = {
-  production: ${environment === 'production'},
-  SUPABASEURL: "${process.env.SUPABASEURL}",
-  SUPABASEAPIKEY: "${process.env.SUPABASEAPIKEY}"
-};
-`;
+const dotenv = require('dotenv');
 
-fs.writeFile(targetPath, envConfigFile, function (err) {
-  if (err) {
-    console.log(err);
-  }
-  console.log(`Archivo de entorno generado en ${targetPath} para el entorno ${environment}`);
-});
+
+
+// Detecta si estás en Appflow (ENV debería estar configurado en las variables de entorno de Appflow)
+
+const isAppflow = !!process.env.ENV && !process.env.LOCAL_ENV;
+
+const envFilePath = isAppflow ? null : '.env';
+
+const environmentFile = (process.env.ENV || 'development') === 'development' ? `environment.ts` : "environment.prod.ts";
+
+
+
+// Carga las variables de entorno desde el archivo local si no está en Appflow
+
+let envConfig = {};
+
+if (!isAppflow && envFilePath) {
+
+    envConfig = dotenv.parse(fs.readFileSync(envFilePath));
+
+} else {
+
+    // Usa process.env directamente en Appflow
+
+    envConfig = {
+
+        PRODUCTION: true,
+
+        API_URL: process.env.API_URL || '',
+
+        API_KEY_SUPABASE: process.env.API_KEY_SUPABASE || '',
+
+    };
+
+}
+
+
+
+// Genera el archivo de entorno para Angular
+
+fs.writeFileSync(
+
+    `./src/environments/${environmentFile}`,
+
+    'export const environment = ' + JSON.stringify(envConfig) + ';'
+
+);
+
+
+
+console.log(`Archivo de entorno generado en ${environmentFile} para el entorno ${process.env.ENV || 'desarrollo'}`);
+
